4ec391ec6cadf30f44d7678644a521b41dbcf3f3
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-21 00:38:09 +0900

    Add binary install

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,9 @@
 set -e
 echo "TODO implement this script."
 echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
-exec gh api user --jq '"You are @\(.login) (\(.name))."'
-mv "build/rust/target/${TARGET}/release/gh-activity" "./dist/${ARTIFACT}"
+cp -v ./gh-mix ./dist
+cp -v ./build/c/gh-mix-c/gh-mix-c ./dist
+cp -v ./build/cpp/gh-mix-cpp/gh-mix-cpp ./dist
+cp -v ./build/rust/gh-mix-rust/release/gh-mix-rust ./dist
+
 exit 0
