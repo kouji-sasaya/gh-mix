04e1596a5525d97ed7a1e15633c77759566925a2
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-27 21:42:46 +0900

    feat: all update

--- cpp/gh-mix-cpp/src/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, cpp programming language!" << std::endl;
-    return 0;
-}
---
+++ gh-mix-ctx2/src/hello.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, workd in Rust!");
+}
